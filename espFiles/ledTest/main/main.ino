c66178440d9c49e3cf9c843a37303026fdf2c644
lor1290 <lor12900000@gmail.com>
2026-04-01 10:11:53 +0200

    esp config file

---
+++ espFiles/ledTest/main/main.ino
@@ -0,0 +1,15 @@
+
+uint8_t redLed = 14;
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(redLed, OUTPUT);
+
+}
+
+void loop() {
+    digitalWrite(redLed, HIGH);
+    delay(500);
+    digitalWrite(redLed, LOW);
+    delay(500);
+}
