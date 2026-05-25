84868113e98747e72580fcaca1406f19fa7536fa
lor1290 <lor12900000@gmail.com>
2026-05-25 22:37:16 +0200

    fix

--- web/project/obj/Debug/net10.0/project.AssemblyInfo.cs
+++ web/project/obj/Debug/net10.0/project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e45aa5cd41a44310028316c58f17bbdb696f8573")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e136c29c2def53f2a81f58d08f42db1e65b5bd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
